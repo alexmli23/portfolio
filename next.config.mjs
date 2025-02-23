5cc6ba9b7996af8c460fb54e032a393c71369650
Alex Li <alexanderli5210@gmail.com>
2025-02-22 19:06:52 -0600

    Hopefully

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/portfolio",
-    assetPrefix: "/portfolio/",
-    trailingSlash: true,
 };
 
 export default nextConfig;
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className="absolute top-0 left-0 w-full z-20 flex justify-between px-10 py-6 text-white/90 text-sm uppercase tracking-wide font-serif">
-      <Link href="" className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">
+      <Link href="/portfolio/" className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">
         <span>Alexander M. Li</span>
       </Link>
       <div className="space-x-6 flex">
-        <Link href="projects" className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">
+        <Link href="/portfolio/projects" className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">
           Projects
         </Link>
         <a href="/Alex_swe_Resume_copy.pdf" 
