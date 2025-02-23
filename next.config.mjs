f03d02a2e4b645e40a57223ce2c6708156a41722
Alex Li <alexanderli5210@gmail.com>
2025-02-22 19:15:39 -0600

    Changed paths for local and production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-};
-
-export default nextConfig;
+    output: 'export', // Required for static export
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  };
+  
+  export default nextConfig; 
