f6f6b11d15615c809b0f95595a65645bfc1bd21a
Alex Li <alexanderli5210@gmail.com>
2025-02-22 18:46:03 -0600

    Adjusted paths for navbar.. AGAIN

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,23 +1,17 @@
 "use client";
-import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <div className="absolute top-0 left-0 w-full z-20 flex justify-between px-10 py-6 text-white/90 text-sm uppercase tracking-wide font-serif">
-      <a
-        href="./portfolio/"
-        className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0"
-        >
+      <Link href="/portfolio/" className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">
         <span>Alexander M. Li</span>
-      </a>
+      </Link>
       <div className="space-x-6 flex">
-        <a
-          href="./projects"
-          className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0"
-          >
+        <Link href="/portfolio/projects" className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">
           Projects
-        </a>
-        <a href="./portfolio/Alex_swe_Resume_copy.pdf" 
+        </Link>
+        <a href="/Alex_swe_Resume_copy.pdf" 
            target="_blank"
            rel="noopener noreferrer"
            className="relative hover:text-white after:content-[''] after:absolute after:left-1/2 after:bottom-0 after:w-0 after:h-[2px] after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:after:left-0">
